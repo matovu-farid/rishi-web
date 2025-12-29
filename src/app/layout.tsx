abd384d3216353c180ff5d0542f32be9a973e76e
matovu-farid <matovu90@gmail.com>
2025-12-29 03:10:30 +0300

    Update PUBLISHABLE_KEY assignment to use NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY for environment variable consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   title: "Rishi",
   description: "Rishi is a reading assistant that helps you read better.",
 };
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
